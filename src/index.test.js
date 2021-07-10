b43326da7649311208bf4a89bad1e33b1858eaa0
yujong-lee <yujonglee@g.skku.edu>
2021-07-10 16:32:12 +0900

    출력내용에 대한 테스트를 추가하라

--- src/index.test.js
+++ src/index.test.js
@@ -10,6 +10,17 @@ describe('statement', () => {
       console.log(statement(invoice, plays));
       console.log('------------------------------------------');
     });
+    
+    const result = `
+    Statement for BigCo
+    Hamlet: $650.00 (55석)
+    As You Like It: $580.00 (35석)
+    Othello: $500.00 (40석)
+    총액: $1,730.00
+    적립 포인트: 47점
+    `
+    
     expect(console.log).toHaveBeenCalledWith('------------------------------------------');
+    expect(console.log.mock.calls[0][0].replace(/\s/g, "")).toBe(result.replace(/\s/g, ""))
   });
 });
