441549c9f78306a1f1d7b2dee6a571ebe8d29c97
davidyang2149 <davidyang2149@gmail.com>
2021-07-11 22:24:39 +0900

    :recycle: [리팩토링] index 테스트코드 수정
    리팩터링 2판 과제
    - index 테스트코드 수정.
    - 기본 테스트코드 리팩토링.

--- src/index.test.js
+++ src/index.test.js
@@ -2,14 +2,22 @@ import statement from './index.js';
 import { invoices, plays } from './fixtures.js';
 import { expect } from '@jest/globals';
 
-describe('statement', () => {
-  it('Test', () => {
-    console.log = jest.fn();
+const wanted =
+  `Statement for BigCo
+ Hamlet: $650.00 (55석)
+ As You Like It: $580.00 (35석)
+ Othello: $500.00 (40석)
+총액: $1,730.00
+적립 포인트: 47점
+`;
 
-    invoices.forEach((invoice) => {
-      console.log(statement(invoice, plays));
-      console.log('------------------------------------------');
+describe('statement', () => {
+  describe('With type value', () => {
+    it('Test', () => {
+      const result = invoices.reduce((acc, invoice) => {
+        return acc += statement(invoice, plays);
+      }, '');
+      expect(result).toBe(wanted);
     });
-    expect(console.log).toHaveBeenCalledWith('------------------------------------------');
   });
 });
